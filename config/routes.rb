ea3cb4d7e0aa3e1e3815237b40ae617f8c73bb86
Alina Vasylieva <alynamarchenk@gmail.com>
2016-10-12 22:45:32 +0200

    task 2 is completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-get '/home' => 'home#index'
+  get '/home' => 'home#index'
+
+  get '/cards' => 'cards#index'
+
+  get '/cards/:id' => 'cards#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
