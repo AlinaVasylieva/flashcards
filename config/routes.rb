5e779af0b945c238b1770c3de76b5fc930168568
Alina Vasylieva <alynamarchenk@gmail.com>
2016-10-13 10:37:52 +0200

    routes.rb is changed restful resource is used

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get '/home' => 'home#index'
 
-  get '/cards' => 'cards#index'
-
-  get '/cards/:id' => 'cards#show'
+  resources :cards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
