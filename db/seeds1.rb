6d5f49203d3736fb891ebe7ac8f716e971d299dd
Alina Vasylieva <alynamarchenk@gmail.com>
2016-10-20 13:31:24 +0200

    Parsing seeds

--- db/seeds1.rb
+++ db/seeds1.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'nokogiri'
+require 'open-uri'
+
+def fetch
+  page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words")) 
+  rows = page.xpath("//*[@id='jsn-mainbody']/div[2]/div/div[1]/table")
+
+  rows_array = []
+
+  rows.each do |tr|
+    original_text = tr.all('td')[1].text
+    translated_text = tr.all('td')[2].text
+    rows_array << Сard.create(original_text, translated_text)
+
+  end
+end
+
+fetch.new
