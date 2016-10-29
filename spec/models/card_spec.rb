e826910336f6566335cdfe34967b9dd4a87598d0
Alina Vasylieva <alynamarchenk@gmail.com>
2016-10-29 16:24:40 +0200

    FactoryGirl config. 3 tests remain failed

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,31 +1,41 @@
 require 'rails_helper'
 
 describe Card do
+
+  before(:all) do
+    Card.destroy_all
+    FactoryGirl.create(:card)
+  end
+
   it "has a valid factory" do
-    FactoryGirl.create(:card).to be_valid
+    card = Card.destroy_all
+    expect(create(:card)).to be_valid
   end
 
   it "is invalid without original_text " do
     card = Card.create(:original_text =>nil)
-    expect(card.errors[:original_text]).to include("Can't be blank")
+    expect(card.errors[:original_text]).to include("can't be blank")
   end
 
   it "is invalid without translated_text " do
     card = Card.create(:translated_text =>nil)
-    expect(card.errors[:translated_text]).to include("Can't be blank")
+    expect(card.errors[:translated_text]).to include("can't be blank")
   end
 
   it "is fetching a review_date" do
-    card = FactoryGirl.create :card
-    expect(card.review_date).to eq(Date.today+3.days)
+    card = Card.destroy_all
+    create(:card)
+    expect(review_date).to eq(Date.today + 3.days)
   end
 
   it "is invalid if original_text equals to translated_text" do
-    card = FactoryGirl.create :card
-    expect(card.original_text).not_to eq(card.translated_text)
-    expect(card.errors[:original_text, :translated_text]).to include("Original text must be different from Translated text")
+    card = Card.create(original_text: 'test', translated_text: 'test') 
+    expect(card.errors).to include("Original text must be different from Translated text")
+    #expect(:original_text).not_to eq(:translated_text)
+    #expect(card.errors[:original_text, :translated_text]).to include("Original text must be different from Translated text")
   end
   
   it "returns a random card by expired date" do
     expect(Card.random_card.to_sql).to include('RANDOM()')
+  end
 end
