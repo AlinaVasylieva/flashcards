578b871261e480c32d5e2a8f6b521f18c7461834
Alina Vasylieva <alynamarchenk@gmail.com>
2016-11-03 16:34:06 +0100

    Another try

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    Card.destroy_all
-    @user = create(:user)
-    @card = create(:card)
-    @user.cards << @card
+    user = create(:user)
+    card = create(:card)
+    user.cards << card
   end
 
   it "has_many cards" do
-    expect(@user).to have_many(@cards)
+    expect(user).to have_many(cards)
   end
 end
 
