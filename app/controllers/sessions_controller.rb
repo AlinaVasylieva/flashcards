0126a48e6f4459498397f9b985c8d25d22a548eb
Alina Vasylieva <alynamarchenk@gmail.com>
2016-11-10 10:52:15 +0100

    Logout fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,8 +22,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    #logout
-    session[:user_id] = nil
+    logout
+    #session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out successfully"
   end
 end
