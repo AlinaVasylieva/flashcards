6f49cc8032df177cd39d6fe26080bc0ad295b96f
Alina Vasylieva <alynamarchenk@gmail.com>
2016-12-23 12:15:49 +0100

    dotenv

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :facebook, '332472093786073', '3ac1ea8328e68fb66451605ca8c0a19a' 
+  provider :facebook, ENV['facebook_key'], ENV['facebook_secret']
 end
