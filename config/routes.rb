50236c2e9eeb1c033efd50a6d2dec95ef7d6f1db
Alina Vasylieva <alynamarchenk@gmail.com>
2016-12-26 11:27:04 +0100

    Env file

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
 
-  get 'auth/:provider/callback', to: "sessions#create"
+  get '/auth/facebook', to: "sessions#create"
   delete 'logout', to: "sessions#destroy", as: 'logout'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
