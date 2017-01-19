325943f8d3b53442fca0228dd09d84b096c644a0
Alina Vasylieva <alynamarchenk@gmail.com>
2017-01-19 22:07:29 +0100

    Generate model and migrate

--- db/migrate/20161103130958_add_user_id_to_cards.rb
+++ db/migrate/20161103130958_add_user_id_to_cards.rb
@@ -1,5 +1,6 @@
 class AddUserIdToCards < ActiveRecord::Migration[5.0]
   def change
-    add_column :cards, :user_id, :integer
+    #add_column :cards, :user_id, :integer
+    add_column :cards, :deck_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109102242) do
+ActiveRecord::Schema.define(version: 20170119210344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20170109102242) do
     t.datetime "updated_at",      null: false
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
